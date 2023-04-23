9a459e349e95cb1ff667dc9eb0e5e52bb1b443b5
domlc <deoliveiracoelholucas210@gmail.com>
2023-04-23 16:27:53 -0300

    Apagando repositorio antigo

--- Api j Throyer/users.api/src/main/java/com/github/domlcforce123/usersapi/repositories/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.github.domlcforce123.usersapi.repositories;
-
-import com.github.domlcforce123.usersapi.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> { }
